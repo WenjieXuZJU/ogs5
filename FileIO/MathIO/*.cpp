8e99a0d76773785f1cd41a6665a9aff20e922ae1
WenjieXuZJU <35688153+WenjieXuZJU@users.noreply.github.com>
2021-03-12 22:27:20 +0800

    Delete *.cpp

--- FileIO/MathIO/*.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * \copyright
- * Copyright (c) 2020, OpenGeoSys Community (http://www.opengeosys.org)
- *            Distributed under a Modified BSD License.
- *              See accompanying file LICENSE.txt or
- *              http://www.opengeosys.org/project/license
- *
- */
-
